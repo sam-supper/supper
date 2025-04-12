06e9d525f001dc70b124aa27e6e61e9febb25371
Logan Sparlin <logan.sparlin@gmail.com>
2025-04-12 03:33:56 -0400

    Update clamp.ts

--- lib/clamp.ts
+++ lib/clamp.ts
@@ -1,3 +1,11 @@
-export const clamp = ({value, min, max}) => {
+interface ClampProps {
+  value: number;
+  min: number;
+  max: number;
+}
+
+export const clamp = (props: ClampProps) => {
+  const { value, min, max } = props;
+
   return Math.min(Math.max(value, min), max)
 }
