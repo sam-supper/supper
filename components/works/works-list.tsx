a0495d0b1aa99d1ffcdb63d1d6f8f16fd6972d1f
Logan Sparlin <logan.sparlin@gmail.com>
2025-05-15 11:45:34 -0400

    Bottom align list image

--- components/works/works-list.tsx
+++ components/works/works-list.tsx
@@ -18,36 +18,38 @@ export const WorksList: FC<WorksListProps> = ({ projects }) => {
 
   return (
     <motion.div
-      className="w-full flex flex-col gap-25 md:min-h-[calc(100svh-390px)] relative"
+      className="w-full md:min-h-[calc(100svh-390px)] relative"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.45, ease: easeInOutQuart }}
     >
-      <div className="w-full site-grid text-list-title italic text-black dark:text-grey transition-colors duration-400 ease">
-        <div className="col-span-4 md:col-span-10 lg:col-span-9 md:grid md:grid-cols-9 gap-x-20">
-          <div className="md:col-span-3">Project:</div>
-          <div className="hidden md:block md:col-span-3">Client:</div>
-          <div className="hidden md:block md:col-span-3">Service:</div>
+      <div className="relative w-full flex flex-col gap-25">
+        <div className="w-full site-grid text-list-title italic text-black dark:text-grey transition-colors duration-400 ease">
+          <div className="col-span-4 md:col-span-10 lg:col-span-9 md:grid md:grid-cols-9 gap-x-20">
+            <div className="md:col-span-3">Project:</div>
+            <div className="hidden md:block md:col-span-3">Client:</div>
+            <div className="hidden md:block md:col-span-3">Service:</div>
+          </div>
+          <div className="col-span-2 lg :col-span-3 text-right md:text-left">
+            Year:
+          </div>
         </div>
-        <div className="col-span-2 lg :col-span-3 text-right md:text-left">
-          Year:
+        <div className="flex flex-col group">
+          {projects.map((project, index) => (
+            <WorksListItem
+              key={project._id}
+              onPointerEnter={() => setHoveredIndex(index)}
+              onPointerLeave={() => setHoveredIndex(null)}
+              {...project}
+            />
+          ))}
         </div>
+        <WorksListHoverImage
+          projects={projects}
+          activeIndex={hoveredIndex}
+        />
       </div>
-      <div className="flex flex-col group">
-        {projects.map((project, index) => (
-          <WorksListItem
-            key={project._id}
-            onPointerEnter={() => setHoveredIndex(index)}
-            onPointerLeave={() => setHoveredIndex(null)}
-            {...project}
-          />
-        ))}
-      </div>
-      <WorksListHoverImage
-        projects={projects}
-        activeIndex={hoveredIndex}
-      />
     </motion.div>
   )
 }
